6f62000f4f3ae22ba1fae5bc757602eab69cca3a
Wesley Wang <wesleywang2@gmail.com>
2019-05-20 20:39:16 -0700

    Add pop, contains, and rind methods

--- linked_list.rb
+++ linked_list.rb
@@ -30,7 +30,6 @@ class LinkedList
   end
 
   def at(index)
-    count = 0
     current_node = @head
     index.times do |i|
       current_node = current_node.pointer
@@ -39,7 +38,36 @@ class LinkedList
     current_node
   end
 
+  def pop
+    new_tail = at(@size - 2)
+    new_tail.pointer = nil
+    @tail = new_tail
+    @size -= 1
+  end
 
+  def contains?(target_value)
+    current_node = @head
+    size.times do
+      if current_node.value == target_value
+        return true
+      end
+      current_node = current_node.pointer
+    end
+
+    false
+  end
+
+  def find(target_value)
+    current_node = @head
+    size.times do |i|
+      if current_node.value == target_value
+        return i
+      end
+      current_node = current_node.pointer
+    end
+
+    nil
+  end
 
   def to_s
     string = ""
@@ -69,5 +97,13 @@ list.append(Node.new(9))
 list.prepend(Node.new(1))
 list.prepend(Node.new(0))
 list.to_s
+list.pop
+list.to_s
+list.pop
+list.to_s
+list.pop
+list.to_s
 
-puts list.at(4).value 
+puts list.find(1)
+puts "----------"
+puts list.find(4)
